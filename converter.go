e98e7de62211dd988db39902499446c8d562b723
NickTacke <the.nick.tacke@gmail.com>
2025-03-06 22:25:35 +0100

    First init with some constants

---
+++ converter.go
@@ -0,0 +1,39 @@
+package excelibur
+
+import "fmt"
+
+const (
+	// Signatures
+	OLE2_SIGNATURE = "\xD0\xCF\x11\xE0\xA1\xB1\x1A\xE1"
+
+	// OLE2 Sector Sizes
+	SECTOR_SIZE      = 512
+	MINI_SECTOR_SIZE = 64
+
+	// BIFF Record Types
+	RECORD_BOF        = 0x0809 // Beginning of File
+	RECORD_EOF        = 0x000A // End of File
+	RECORD_BOUNDSHEET = 0x0085 // Worksheet information
+	RECORD_SST        = 0x00FC // Shared String Table
+	RECORD_LABELSST   = 0x00FD // Cell with string from SST
+	RECORD_NUMBER     = 0x0203 // Cell with number data
+	RECORD_FORMULA    = 0x0006 // Cell with formula
+	RECORD_STRING     = 0x0207 // Result of string formula
+	RECORD_BOOLERR    = 0x0205 // Cell with boolean or error
+	RECORD_FORMAT     = 0x041E // Number format definition
+	RECORD_XF         = 0x00E0 // Cell formatting
+	RECORD_MULRK      = 0x00BD // Multiple RK cells
+	RECORD_RK         = 0x027E // Cell with RK value
+	RECORD_BLANK      = 0x0201 // Empty cell
+	RECORD_ROW        = 0x0208 // Row properties
+)
+
+type XLSReader struct {
+	// Configuration parameters
+	IgnoreErrors bool
+	Debug        bool
+}
+
+func (xls *XLSReader) ConvertFile(xlsFile string, xlsxOut string) error {
+	return fmt.Errorf("not implemented")
+}
